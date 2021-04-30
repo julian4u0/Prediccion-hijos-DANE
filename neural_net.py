a4c8960ce4e8f5d3ea3faca1f535ac97b42961f5
Dieguinho1612 <53007033+Dieguinho1612@users.noreply.github.com>
2021-04-30 18:29:35 -0400

    Add files via upload

--- neural_net.py
+++ neural_net.py
@@ -3,7 +3,7 @@ Input: Different attributes of a Household as a vector
 Output: The estimated number of children in that household as a real number
 Architecture: 1 Hidden-Layer with the Activaction Function Leaky ReLU, Outputlayer has Dimension 1 and no Activation Function
 Cost-function: Mean-Squared Error
-Training/Testing: Use k-fold cross validation
+Training/Testing: 60.000 Training Samples, 28713 Test Samples
 
 The Dataset consists the following columns:
 1 ID of Instance
@@ -35,11 +35,17 @@ data_temp = []
 for instance in f:
 	data_temp.append(instance.split(","))
 
-#replace all entries "NA" with zero, such that column 5 becomes a numerical variable
+#replace categorical variables with integer numbers
+ethnicities = []
 for a in range(len(data_temp)):
-	for b in range(len(data_temp[0])):
-		if data_temp[a][b] == "NA":
-			data_temp[a][b] = 0
+	
+	if data_temp[a][5] == "NA":
+		data_temp[a][5] = 0
+
+	ethn = data_temp[a][6]
+	if ethn not in ethnicities:
+		ethnicities.append(ethn)
+	data_temp[a][6] = ethnicities.index(ethn)
 
 #transform list of lists into array/matrix. It has dimension m=88713x11, so every row is a houshold
 data_temp = np.array(data_temp)
@@ -50,23 +56,31 @@ np.random.seed(0)
 np.random.shuffle(data_temp)
 
 #choose columns (attributes of households) that should be considered, create matriz and transpose it
-X_data = data_temp[:,[2,3,4,5,8,9,10]].T #choose columns that shall be considered			
+X_data = data_temp[:,[2,3,4,5,6,9,10]].T #choose columns that shall be considered		
 X_data = np.vectorize(float)(X_data)
+#split into training and test set, 60.000 instances go into the training set, the rest into the test set
+X_train = X_data[:,:60000]
+X_test = X_data[:,60000:]
 #standardize
-X_mean = np.mean(X_data, axis=1).reshape(X_data.shape[0],1)
-
-
-X_std = np.std(X_data, axis=1).reshape(X_data.shape[0],1)
+X_mean = np.mean(X_train, axis=1).reshape(X_data.shape[0],1)
+X_std = np.std(X_train, axis=1).reshape(X_data.shape[0],1)
 X_data = (X_data - X_mean) / X_std
+X_train = (X_train - X_mean) / X_std
+X_test = (X_test - X_mean) / X_std
 #so X-data is (n_x,m)-matrix, where every column is a vector containing all attributes of one instance
 
 #choose column 7, containing the number of children of an household which will be our label. Create matriz and transpose it
-Y_data =data_temp[:,[7]].T
+Y_data = data_temp[:,[7]].T
 Y_data = np.vectorize(float)(Y_data)
+#split into training and test set, 60.000 instances go into the training set, the rest into the test set
+Y_train = Y_data[:,:60000]
+Y_test = Y_data[:,60000:]
 #standardize
-Y_mean = np.mean(Y_data, axis=1) 
-Y_std = np.std(Y_data, axis=1)
-Y_data =  (Y_data - Y_mean) / Y_std
+Y_mean = np.mean(Y_train, axis=1) 
+Y_std = np.std(Y_train, axis=1)
+Y_data = (Y_data - X_mean) / X_std
+Y_train =  (Y_train - Y_mean) / Y_std
+Y_test =  (Y_test - Y_mean) / Y_std
 #Y_data is (1,m)-matrix containing the labels (number of children) of the instances/households
 
 #(n_x+1,m)-matrix where every column is a vector containing all attributes and the label of one instance
@@ -75,8 +89,8 @@ Data = np.append(X_data, Y_data, axis=0)
 
 
 #Dimensions
-n_x = len(X_data)
-m = len(X_data[0])
+n_x = len(X_train)
+m = len(X_train[0])
 
 #Activation_function
 def leaky_relu(Z):
@@ -88,8 +102,9 @@ def leaky_relu(Z):
 def leaky_relu_backwards(Z):
 	def der_elementwise(Z):
 		if Z < 0:
-			Z *= 0.01
-		return(Z)
+			return (0.01)
+		else:
+			return (1)
 	return(np.vectorize(der_elementwise)(Z))
 
 #Initialize parameters w and b
@@ -124,17 +139,19 @@ def two_layer_forward(X, parameters):
 	W1 = parameters["W1"]
 	b1 = parameters["b1"]
 	A1, cache = activation_forward(X,W1,b1,"leaky_relu")
+	#print("A1 = " + str(A1))
 	caches.append(cache)
 
 	W2 = parameters["W2"]
 	b2 = parameters["b2"]
 	A2, cache = activation_forward(A1,W2,b2,"")
+	#print("A2 = " + str(A2))
 	caches.append(cache)
 
 	Y_hat = A2
 	return(Y_hat,caches)
 
-#Cost-functions is the calculation of the mean-squared-error
+#Cost-functions
 def cost_func(Y,Y_hat):
 	m = len(Y_hat[0])
 	cost = 1/m * np.sum(np.power(Y-Y_hat,2))
@@ -152,6 +169,8 @@ def linear_activation_backward(dA, cache, activation):
 	linear_cache, activation_cache = cache
 	if activation == "leaky_relu":
 		dZ = dA * leaky_relu_backwards(activation_cache)
+		#print("dA check = " + str(dA))
+		#print("dZ = " +str(dZ))
 	elif activation == "":
 		dZ = dA * 1
 	dA_prev, dW, db = linear_backward(dZ,linear_cache)
@@ -160,16 +179,21 @@ def linear_activation_backward(dA, cache, activation):
 #backward-propagation
 def two_layer_backward(A2,Y, caches):
 	grads = {}
-	dA2 = -2/m * (Y-A2)
+	dA2 = (-2/m) * (Y-A2)
+	#print("dA2 = " + str(dA2))
 
 	dA_prev_temp, dW_temp, db_temp = linear_activation_backward(dA2, caches[1], "")
 	grads["dA1"] = dA_prev_temp
 	grads["dW2"] = dW_temp
 	grads["db2"] = db_temp
+	#print("dW2 = " + str(grads["dW2"]))
+	#print("")
+	#print("db2 = " + str(grads["db2"]))
 
 	dA_prev_temp, dW_temp, db_temp = linear_activation_backward(grads["dA1"], caches[0], "leaky_relu")
 	grads["dW1"] = dW_temp
 	grads["db1"] = db_temp
+
 
 	return grads
 
@@ -209,6 +233,58 @@ def save_parameters(parameters):
 	pickle.dump(parameters,f)
 	f.close()
 
+#check if gradients where computed correctly
+def gradient_checking(X,Y,parameters, grads):
+	
+	epsilon = 10**(-7)
+	
+	W1 = parameters["W1"]
+	W2 = parameters["W2"]
+	b1 = parameters["b1"]
+	b2 = parameters["b2"]
+
+	W1_flat = np.matrix.flatten(np.copy(W1))
+	W2_flat = np.matrix.flatten(np.copy(W2))
+	b1_flat = np.matrix.flatten(np.copy(b1))
+	b2_flat = np.matrix.flatten(np.copy(b2))	
+	parameter = np.hstack((W1_flat,b1_flat,W2_flat,b2_flat))
+
+	dW1 = np.matrix.flatten(np.copy(grads["dW1"]))
+	dW2 = np.matrix.flatten(np.copy(grads["dW2"]))
+	db1 = np.matrix.flatten(np.copy(grads["db1"]))
+	db2 = np.matrix.flatten(np.copy(grads["db2"]))
+	gradient = np.hstack((dW1,db1,dW2,db2))
+
+
+	d_approx = np.zeros(len(gradient))
+	index_add = 0
+
+	dict_entries = ["W1","b1","W2","b2"]
+
+	for entry in dict_entries:
+		param = parameters[entry]
+		for i in range(len(param)):
+			for j in range(len(param[0])):
+				param_plus = np.copy(param)
+				param_plus[i][j] = param[i][j] + epsilon
+				parameters_plus = dict(parameters)
+				parameters_plus[entry] = param_plus
+				
+				param_minus = np.copy(param)
+				param_minus[i][j] = param[i][j] - epsilon
+				parameters_minus = dict(parameters)
+				parameters_minus[entry] = param_minus
+				
+				Y_hat_plus,__ = two_layer_forward(X, parameters_plus)
+				cost_plus = cost_func(Y,Y_hat_plus)
+				
+				Y_hat_minus,__ = two_layer_forward(X, parameters_minus)
+				cost_minus = cost_func(Y,Y_hat_minus)
+				
+				d_approx[index_add + i*len(param[0])+j] = (cost_plus - cost_minus) / (2*epsilon)
+
+		index_add += len(param) * len(param[0]) 
+
 #combine everything to the neural network
 def neural_net(X,Y,n_h,learning_rate,epochs, cost_goal = 0, print_cost = False, read = False, save = False):
 
@@ -222,14 +298,19 @@ def neural_net(X,Y,n_h,learning_rate,epochs, cost_goal = 0, print_cost = False, 
 		#compute predicted labels in every iteration and store cache for backward-propagation
 		Y_hat,caches = two_layer_forward(X, parameters)
 	
-		#show costs (equal to mean-squared-error) to control convergence
+		#show costs to control convergence
 		cost = cost_func(Y,Y_hat)
+		#print("cost = " + str(cost))
 		if print_cost == True:
 			if epoch % 10 == 0:
 				print("epoch = " + str(epoch), "cost = " + str(cost))						
 
 		#save gradients for gradient-descent
 		grads = two_layer_backward(Y_hat,Y,caches)
+
+		#if needed: check gradients
+		#if epoch == 2:
+			#gradient_checking(X,Y,parameters, grads)
 
 		#update parameters via gradient descent
 		parameters = update_parameters(parameters,grads,learning_rate)
@@ -253,6 +334,18 @@ def neural_net(X,Y,n_h,learning_rate,epochs, cost_goal = 0, print_cost = False, 
 		save_parameters(parameters)
 	
 	return (parameters)
+
+#Test learned parameters on test set
+def test_model(X,Y):
+	#gain learned parameters
+	parameters = read_parameters()
+	#run neural entwork to make prediction
+	Y_hat,__ = two_layer_forward(X, parameters)
+	#calculate costs
+	cost = cost_func(Y,Y_hat)
+
+	print(cost)
+	return (cost)
 
 #k-fold
 def fold_data(i,k):
@@ -341,31 +434,13 @@ def alt_cross_validate(k,n_h,learning_rate,epochs, least_cost_goal = 0, print_co
 	print("least cost = "+str(cost))
 	return(parameters)
 
-
-
-import sys
-genero = sys.argv[1]
-edad = sys.argv[2]
-estadocivil = sys.argv[3]
-a = sys.argv[4]
-personas = sys.argv[5] # numero de personas que viven en el hogar
-ingresos = sys.argv[6] #ingresos del hogar
-cuartos = sys.argv[7] #cuartos
-
-
-
-# 1 Sex of the chef of the house
-# 2 Age of this chef
-# 3 Marital status of this chef
-# 4 Stands for weather the spouse lives in the same household or not
-# 5 Number of persons living in the household
-# 6 Income of the household
-# 7 Number of rooms in which the persons of the household sleep
-
 #one can import the following function from a different file to use the neural network and the obtained parameters from this file to make a prediction on new date
 def label_new_data(X1, X2, X3, X4, X5, X6, X7):
+	#reshape X
 	X = np.array([X1, X2, X3, X4, X5, X6, X7])
 	X = X.reshape((7,1))
+	#categorize Ethnicity
+	X[4,0] = ethnicities.index(X[3,0])
 	#standardize X
 	X = (X - X_mean) / X_std
 	#call learned parameters
